65a774c07b4ec1f46cf7594ccbbc5f9cb0732a75
Knightsurfer <armstrong5454@msn.com>
2018-11-10 01:40:57 +0000

    Started on som java

---
+++ assets/scripts/javastuff.js
@@ -0,0 +1,27 @@
+		var buttonClicked = 0;
+		function buttonClick()
+		{
+			var x = document.getElementById("javatest");
+			buttonClicked ++;
+			
+			if(buttonClicked == 1)
+			{
+				x.innerHTML += "<br>The button worked!!";
+				//x.innerHTML += "<br><br>" + Date();
+			}
+			
+			
+			if(buttonClicked == 2)
+			{
+				x.innerHTML += "<br>YOU PRESSED ME TOO MANY TIMES!!! >:(";
+			}
+			
+			
+		}
+		
+		function buttonClick()
+		{
+			
+		}
+		
+		
